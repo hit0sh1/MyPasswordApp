230d572f6ccfbd07e65852af3e99377f5349a2d2
hit0sh1 <h.miyagawa365@gmail.com>
2024-01-20 23:29:13 +0900

    ✨ javascript導入まで

---
+++ main.js
@@ -0,0 +1,18 @@
+"use strict";
+{
+    const slider = document.getElementById("slider");
+    const btn = document.getElementById("btn");
+
+    slider.addEventListener("input", () => {
+        const passwordLength = document.getElementById("password-length");
+
+        passwordLength.textContent = slider.value;
+    });
+
+    btn.addEventListener("click", () => {
+        const result = document.getElementById("result");
+        const letters = "abcdefghijklmnopqrstuvwxyz";
+
+        result.textContent = letters[Math.floor(Math.random() * 26)];
+    });
+}
